0b870a9efe8b0a5f14238ed277e21972e5e18505
asher_wang998 <hi_wangtao@163.com>
2022-11-27 11:03:51 +0800

    added Check

---
+++ Check.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Check {
+    public boolean check(int[] nums) {
+        int cnt = 0;
+        for (int i = 0, n = nums.length; i < n; ++i) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                ++cnt;
+            }
+        }
+        return cnt <= 1;
+    }
+
+}
